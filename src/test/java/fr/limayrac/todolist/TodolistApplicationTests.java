19085c549ca5dc8a2b9190bfbee08cd9634e0058
AngelVigliengo <angel.vigliengo@limayrac.fr>
2022-01-23 20:41:14 +0100

    Initial commit

---
+++ src/test/java/fr/limayrac/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.limayrac.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
